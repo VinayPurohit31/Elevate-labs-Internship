e0cf9a4c3fb1f84911ddeeb199d14ebb9c8e25e0
VinayPurohit31 <vinaypurohit31@gmail.com>
2025-06-23 17:00:37 +0530

    Created a ArrayList for Sotring the hsitory

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,10 +1,12 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class SimpleCalculator {
 
     public static void main(String[] args) {
-        LogicHandeling LG=new LogicHandeling();
+        LogicHandeling LG = new LogicHandeling();
         System.out.println("------------------Calculator Menu------------------");
         LG.loopLogic();
     }
@@ -21,41 +23,55 @@ class LogicHandeling {
         try {
 
             for (;;) {
-                
+
                 System.out.println("Enter the no for which operation you want to perform ");
                 System.out.println("         1 if you want to add two no ");
                 System.out.println("         2 if you want to sub two no ");
                 System.out.println("         3 if you want to dev two no ");
                 System.out.println("         4 if you want to mul two no ");
-                System.out.print("         5 if you want to terminate:- ");
+                System.out.println("         5 if you want to view the History  ");
+                System.out.println("         6 if you want to terminate the program");
+                System.out.print("         Enter the no :- ");
                 int selectCase = sc.nextInt();
                 System.out.println("");
-                if (selectCase == 5) {
-                    System.out.println("You terminated the session.");
+                if (selectCase == 6) {
+                    System.out.println("       You terminated the session.");
+                    System.out.println("");
                     break;
                 }
-                if (selectCase > 5 || selectCase < 1) {
-                    System.out.println("Illigle value ReEnter the value ");
+                if (selectCase == 5) {
+                    Cf.history();
                     continue;
                 }
-                System.out.print("Enter the first no  :- ");
-                double userInpur1 = sc.nextDouble();
-                System.out.print("Enter the second no :- ");
-                double userInpur2 = sc.nextDouble();
+                if (selectCase >= 1 && selectCase <= 4) {
+                    System.out.print("Enter the first no  :- ");
+                    double userInpur1 = sc.nextDouble();
+                    System.out.print("Enter the second no :- ");
+                    double userInpur2 = sc.nextDouble();
+                    System.out.println("");
 
-                System.out.println("");
+                    System.out.println("");
 
-                switch (selectCase) {
-                    case 1 ->
-                        Cf.add(userInpur1, userInpur2);
-                    case 2 ->
-                        Cf.sub(userInpur1, userInpur2);
-                    case 3 ->
-                        Cf.dev(userInpur1, userInpur2);
-                    case 4 ->
-                        Cf.mult(userInpur1, userInpur2);
-                    default ->
-                        System.out.println("Illigle value choes between 1 to 5 ");
+                    switch (selectCase) {
+                        case 1:
+                            Cf.add(userInpur1, userInpur2);
+                            break;
+                        case 2:
+                            Cf.sub(userInpur1, userInpur2);
+                            break;
+                        case 3:
+                            Cf.dev(userInpur1, userInpur2);
+                            break;
+                        case 4:
+                            Cf.mult(userInpur1, userInpur2);
+                            break;
+
+                        default:
+                            System.out.println("Illigle value choes between 1 to 5 ");
+                    }
+                } else if (selectCase < 1 || selectCase > 6) {
+                    System.out.println("      Illigle value ReEnter the value ");
+                    System.out.println("");
                 }
 
             }
@@ -71,11 +87,14 @@ class LogicHandeling {
 
 class Calculotorfunction {
 
+    List<String> History = new ArrayList<>();
+
     void add(double userInpur1, double userInpur2) {
         double a = userInpur1;
         double b = userInpur2;
         double Addition = a + b;
         System.out.println("Addition of Two no is = " + Addition);
+        History.add("Addition of " + a + " and " + b + " is " + Addition);
         System.out.println("");
 
     }
@@ -85,6 +104,7 @@ class Calculotorfunction {
         double b = userInpur2;
         double Substraction = a - b;
         System.out.println("Substraction of Two no is = " + Substraction);
+        History.add("Substraction of " + a + " and " + b + " is " + Substraction);
         System.out.println("");
     }
 
@@ -95,6 +115,7 @@ class Calculotorfunction {
         if (userInpur2 != 0) {
             double Devision = a / b;
             System.out.println("Devision of Two no is = " + Devision);
+            History.add("Devision of " + a + " and " + b + " is " + Devision);
             System.out.println("");
         } else {
             System.out.println("Divide by 0 gives an Arathmatis Exception ");
@@ -108,6 +129,14 @@ class Calculotorfunction {
         double b = userInpur2;
         double Multiplication = a * b;
         System.out.println("Multiplication of Two no is = " + Multiplication);
+        History.add("Multiplication of " + a + " and " + b + " is " + Multiplication);
         System.out.println("");
+    }
+
+    void history() {
+        System.out.println("---------History of your records--------- ");
+        for (String elem : History) {
+            System.out.println(elem);
+        }
     }
 }
